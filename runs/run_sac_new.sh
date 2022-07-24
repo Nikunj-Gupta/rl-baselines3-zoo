462465db62fc3c56e0c3262c6a428b7c3883e674
Nikunj Gupta <guptanikunj1997@gmail.com>
2022-07-24 18:55:53 +0530

    new runs

---
+++ runs/run_sac_new.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#SBATCH --nodes=1
+#SBATCH --cpus-per-task=16 
+#SBATCH --time=120:00:00
+#SBATCH --mem=256GB
+#SBATCH --job-name=epipolicy_hyperparameter_opt_sac2_new
+#SBATCH --output=epipolicy_hyperparameter_opt_sac2_new.out
+
+source ./venv/bin/activate
+
+module load python/intel/3.8.6
+module load openmpi/intel/4.0.5
+
+time python3 train.py --algo sac --env EpiEnv-v0 -n 30000 -optimize --sampler tpe --pruner median --optimization-log-path summaries/EpiEnv-v0_new/sac/ --tensorboard-log summaries/sac_new --n-trials 1000 --n-jobs 2 --eval-freq 10000 --eval-episodes 10 --n-eval-envs 1 --save-freq 10000 
